3b4b7ada1598b2a4a3d7e607577f745777e5b28a
mioynx <mioynx@example.com>
2026-08-07 18:14:28 +0700

    mio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "FormulaDeta",
+  title: "Formula Data",
   description: "F1 Data and Telemetry Platform",
   verification: {
     google: "wRwGwt4Cmi-4UpGY2YHnDKE4VkY0j-RiyPfsZr6gbl0",
